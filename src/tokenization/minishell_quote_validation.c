bcb68a0feed0f3dede2b2441657a3308aa906d48
Johnny Varila <jvarila@student.hive.fi>
2025-03-11 17:22:58 +0200

    Implemented a quote validation function and a simple test to try it out.

---
+++ src/tokenization/minishell_quote_validation.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell_quote_validation.c                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvarila <jvarila@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 16:47:03 by jvarila           #+#    #+#             */
+/*   Updated: 2025/03/11 17:02:26 by jvarila          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+bool	has_unclosed_quotes(const char *str)
+{
+	if (!str)
+		return (false);
+	while (*str)
+	{
+		if (*str != '\'' && *str != '"')
+		{
+			str++;
+			continue ;
+		}
+		str = ft_strchr(str + 1, *str);
+		if (!str)
+			return (true);
+		else
+			str++;
+	}
+	return (false);
+}
