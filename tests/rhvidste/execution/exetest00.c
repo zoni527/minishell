f3280e4fd4ff60aa68fe6e94e718d03a813fbffc
Ross Hvidsten <rhvidste@student.hive.fi>
2025-03-18 15:19:58 +0200

    some changes to exec test, including proper rerouting and echo, no expansion yet

--- tests/rhvidste/execution/exetest00.c
+++ tests/rhvidste/execution/exetest00.c
@@ -14,6 +14,8 @@
 
 //###############################################PROGRM FUNTIONS########################################################################
 
+
+
 // Function to print the current directory path
 int	get_current_dir(void)
 {
@@ -21,7 +23,7 @@ int	get_current_dir(void)
 	
 	if (buffer != NULL)
 	{
-		printf("Current working directory is: %s\n", buffer);
+		printf("%s\n", buffer);
 		free (buffer);
 	}
 	else
@@ -30,87 +32,20 @@ int	get_current_dir(void)
 		return (1);
 	}
 	return (0);
-//	char buffer[1024];
-//
-//	if (getcwd(buffer, sizeof(buffer)) !=NULL)
-//	{
-//		printf("Current working directory: %s\n", buffer);
-//	}
-//	else
-//	{
-//		perror("getcwd error");
-//	}
-//	return (0);
 }
 
 // FUnction to switch dir
 int	change_dir(char *str)
 {
-//	if (chdir(str) == 0)
-//	{
-//		printf("Directory changed successfully\n");
-//	}
-//	else
-//	{
-//		perror("chdir() error");
-//		return (1);
-//	}
 	if (chdir(str) == -1)
 	{
 		perror("chdir() error");
 		return (1);
 	}
 	return (0);
-//	int	result;
-//	result = chdir(str);
-//	if (result == 0)
-//	{
-//		printf("Directory changed successfully\n");
-//	}
-//	else 
-//	{
-//		perror("Error changing directory");
-//		return (1);
-//	}
-//	return (0);
 }
 
-// Function to check wether its one of the builtins.
-int	check_if_builtin(const char *str)
-{
-	if (ft_strncmp("echo", str, 4) == 0)
-		return (0);
-	if (ft_strncmp("cd", str, 2) == 0)
-		return (0);
-	if (ft_strncmp("pwd", str, 3) == 0)
-		return (0);
-	if (ft_strncmp("export", str, 6) == 0)
-		return (0);
-	if (ft_strncmp("unset", str, 5) == 0)
-		return (0);
-	if (ft_strncmp("env", str, 3) == 0)
-		return (0);
-	if (ft_strncmp("exit", str, 4) == 0)
-		return (0);
-	return (1);
-}
-
-// Function to check wether the build in "echo" is called
-int	check_if_echo(const char *str)
-{
-	if (ft_strncmp("cd", str, 2) == 0)
-		return (0);
-	return (1);
-}
-
-// Function to check wether the built in "cd" is called
-int	check_if_cd(const char *str)
-{
-	if (ft_strncmp("cd", str, 2) == 0)
-		return (0);
-	return (1);
-}
-
+//Function to free an array
 int	free_array(char **arr)
 {
 	int	i = 0;
@@ -211,7 +146,7 @@ void	cmd_exec(char *argv, char **envp)
 int	run_prog(char *input, char **envp)
 {
 	(void)input;
-	(void)envp;
+//	(void)envp;
 	pid_t	pid;
 
 	pid = fork();
@@ -243,20 +178,202 @@ int	run_prog(char *input, char **envp)
 	}
 	return (0);
 }
+////Function to remove single and double quotes from a string
+//char	*remove_quotes(char *str)
+//{
+//	int	i;
+//	int	j;
+//	int	count;
+//	char	*dest;
+//
+//	count = 0;
+//	i = -1;
+//	j = 0;
+//	while (str[++i])
+//	{
+//		if (str[i] == '"' || str[i] == '\'')
+//			count++;
+//	}
+//	dest = malloc(sizeof(char) * (i - count) + 1);
+//	if (dest == NULL)
+//		return (NULL);
+//	i = -1;
+//	while(str[++i])
+//	{
+//		if (str[i] != '"' && str[i] != '\'')
+//			dest[j++] = str[i];
+//	}
+//	dest[j] = '\0';
+//	return (dest);
+//}
+
+//Function to remove single and double quotes from a string
+char	*remove_quotes(char *str)
+{
+	int		i;
+	int		j;
+	int		count;
+	int		in_double_quotes;
+	char	*dest;
+	
+	in_double_quotes = 0;
+	count = 0;
+	i = -1;
+	j = 0;
+	while (str[++i])
+	{
+		if (str[i] == '"')
+		{
+			in_double_quotes = !in_double_quotes;
+			count++;
+		}
+		else if(str[i] == '\'' && !in_double_quotes)
+			count++;	
+	}
+	dest = malloc(sizeof(char) * (i - count) + 1);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	in_double_quotes = 0;
+	while(str[i])
+	{
+		if (str[i] == '"')
+		{
+			in_double_quotes = !in_double_quotes;
+			i++;
+		}
+		else if (str[i] == '\'' && !in_double_quotes)
+		{
+			i++;
+		}
+		else
+			dest[j++] = str[i++];
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+// Function to run ECHO builtin
+void	echo(char *input)
+{
+	char	*sub_str;
+	char	*n_str;
+	char	*no_quotes;
+	sub_str = ft_substr(input, 5, ft_strlen(input) - 4);
+//	printf("substr is %s\n", sub_str);
+	if (sub_str[0] == '-' && sub_str[1] == 'n')
+	{
+//		printf("-n flag detected\n");
+		n_str = ft_substr(sub_str, 3, ft_strlen(sub_str) -2);
+//		printf("n_string:%s\n", n_str);
+		no_quotes = remove_quotes(n_str);
+		free(n_str);
+		ft_putstr_fd(no_quotes, 1);
+		free(no_quotes);
+	}
+	else
+	{
+//		printf("no -n flag detected\n");
+		no_quotes = remove_quotes(sub_str);
+		ft_putendl_fd(no_quotes, 1);
+		free(no_quotes);
+	}
+	free(sub_str);
+}
+//Function to run CD builtin
+void	cd(char *input, char **envp)
+{
+	char	*path;
+	char	*new_path;
+	char	*home_path;
+	char	*new_home_path;
+	(void)envp;
+
+	path = ft_substr(input, 3, (ft_strlen(input) - 2));
+//	printf("path = %s\n", path);
+	if (path[0] == '~')
+	{
+		new_path = ft_substr(path, 1, (ft_strlen(path) - 1));
+//		printf("path == '~'\n");
+		home_path = getenv("HOME");
+//		printf("newpath = %s%s\n", home_path, new_path);
+		new_home_path = ft_strjoin(home_path, new_path);
+		change_dir(new_home_path);
+		free(new_home_path);
+		free(new_path);
+	}
+	//Here we check is CD is just added on its own.
+	else if (path[0] == '\0')
+	{
+//		printf("CD COMMAND ENTERED\n");
+		home_path = getenv("HOME");
+		change_dir(home_path);
+	}
+	else
+	{
+//		printf("cd detected\n");
+		change_dir(path);
+	}
+	free(path);
+}
+
+// Function to call the ENV builtin
+void	env(char **envp)
+{
+	int	i;
+	i = -1;
+	while (envp[++i])
+	{
+		ft_putendl_fd(envp[i], 1);
+	}
+}
+
+// Function to check wether its one of the builtins.
+int	check_if_builtin(char *input)
+{
+	if (ft_strncmp("echo", input, 4) == 0)
+		return (0);
+	if (ft_strncmp("cd", input, 2) == 0)
+		return (0);
+	if (ft_strncmp("pwd", input, 3) == 0)
+		return (0);
+	if (ft_strncmp("export", input, 6) == 0)
+		return (0);
+	if (ft_strncmp("unset", input, 5) == 0)
+		return (0);
+	if (ft_strncmp("env", input, 3) == 0)
+		return (0);
+	if (ft_strncmp("exit", input, 4) == 0)
+		return (0);
+	return (1);
+}
+
+// Function to reroute to relative built in functions
+void	reroute_builtin(char *str, char **envp)
+{
+	if (ft_strncmp("echo", str, 4) == 0)
+		echo(str);
+	if (ft_strncmp("cd", str, 2) == 0)
+		cd(str, envp);
+	if (ft_strncmp("pwd", str, 3) == 0)
+		get_current_dir();
+	if (ft_strncmp("export", str, 6) == 0)
+		printf("EXPORT CALLED\n");
+	if (ft_strncmp("unset", str, 5) == 0)
+		printf("UNSET CALLED\n");
+	if (ft_strncmp("env", str, 3) == 0)
+		env(envp);
+//		printf("ENV CALLED");
+	if (ft_strncmp("exit", str, 4) == 0)
+		printf("EXIT CALLED");
+}
 
 int	main(int argc, char **argv, char **envp)
 {
 	(void)argc;
 	(void)argv;
 //	(void)envp;
-	
-//	get_current_dir();
-
 	char	*input;
-	char	*path;
-	char	*new_path;
-	char	*home_path;
-	char	*new_home_path;
 	char	*shell_dir;
 
 	while (1)
@@ -275,78 +392,16 @@ int	main(int argc, char **argv, char **envp)
 			free(input);
 			break ;
 		}
-//		add_history(input);
-//		printf("output of checkifcd is %d\n", check_if_cd(input));
 		if (check_if_builtin(input) == 0)
 		{
-			if (check_if_echo(input) == 0)
-			{
-				printf("ECHO called\n");
-			}
-			if (check_if_cd(input) == 0)
-			{
-				path = ft_substr(input, 3, (ft_strlen(input) - 2));
-//				printf("path = %s\n", path);
-				if (path[0] == '~')
-				{
-					new_path = ft_substr(path, 1, (ft_strlen(path) - 1));
-//					printf("path == '~'\n");
-					home_path = getenv("HOME");
-//					printf("newpath = %s%s\n", home_path, new_path);
-					new_home_path = ft_strjoin(home_path, new_path);
-					change_dir(new_home_path);
-					free(new_home_path);
-					free(new_path);
-				}
-				//Here we check is CD is just added on its own.
-				else if (path[0] == '\0')
-				{
-//					printf("CD COMMAND ENTERED\n");
-					home_path = getenv("HOME");
-					change_dir(home_path);
-				}
-				else
-				{
-//					printf("cd detected\n");
-					change_dir(path);
-//					input = readline("<< ");
-//					printf("substring out : %s\n",ft_substr(input, 2, (ft_strlen(input) - 2)));
-				}
-				free(path);
-			}
-			else
-			{
-				run_prog(input, envp);
-			}
+//			printf("buildin detected\n");
+			reroute_builtin(input, envp);
 		}
-//		change_dir(input);
-//		add_history(input);
-//		get_current_dir();
-//		if (check_if_cd(input) != 0)
-//		{
-//			printf("argument is not cd\n");
-//			run_prog(input, envp);
-//			if (ft_strncmp("nvim", input, 4) == 0)
-//			{
-//				printf("keyword vim found\n");
-//				run_prog(input, envp);
-//			}
-//		}
+		else
+		{
+			run_prog(input, envp);
+		}
 		add_history(input);
-//		printf("you shouted: %s into the void\n", input);
-//		add_history(input);
-//		free(input);
-//		printf("the void shouts back the following:\n");
-//		print_history();
-//		if (ft_strrchr(input, '<'))
-//		{
-//			printf("%s includes a '<' character\n", input);
-//		}
-//		free(input);
 	}
-
-//	get_current_dir();
-//	change_dir(argv[1]);
-//	get_current_dir();
 	return (0);
 }
