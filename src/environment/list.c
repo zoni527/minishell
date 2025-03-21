2faf4fd52eefacb991c4e242b76ca1a6a8ade3de
Ross Hvidsten <rhvidste@student.hive.fi>
2025-03-21 17:31:46 +0200

    starting to try and implament env stuff into the project

---
+++ src/environment/list.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 17:10:17 by rhvidste          #+#    #+#             */
+/*   Updated: 2025/03/21 17:10:18 by rhvidste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * Function that returns the length of a list
+ *
+ * @param begin_list	pointer to the beginning of the list
+ */
+int	get_list_size(t_var *begin)
+{
+	if (begin == 0)
+		return (0);
+	else
+		return (1 + get_list_size(begin->next));
+}
+
+/**
+ * Function that frees the enviroment list
+ *
+ * @param head	pointer to the head of the list
+ */
+void	free_list(t_var *head)
+{
+	while(head)
+	{
+		t_var *temp = head;
+		free(temp->raw);
+		free(temp->key);
+		free(temp->value);
+		head = head->next;
+		free(temp);
+	}
+}
+
+/**
+ * Function that prints env from
+ * linked list
+ *
+ * @param list	list to print contents of
+ */
+void	print_env_list(t_var *list)
+{
+	t_var *current = list;
+	while (current)
+	{
+		printf("%s\n", current->raw);
+		current = current->next;
+	}
+}
+
+/**
+ * Function appends node and adds values
+ * variable it finds.
+ *
+ * @param data	Main data struct
+ * @param raw	Raw input
+ * @param key	Key input
+ * @param value	Value input
+ */
+t_var	*append_var(t_minishell *data, t_var *prev, \
+				char *raw, char *key, char *value)
+{
+//	t_var *new_node = (t_var *)malloc(sizeof(t_var));
+	t_var	*new_node;
+	new_node = ft_ma_malloc(data->arena, sizeof(t_var));
+	new_node->raw = raw;
+	new_node->key = key;
+	new_node->value = value;
+	new_node->next = NULL;
+	new_node->prev = prev;
+	return (new_node);
+}
